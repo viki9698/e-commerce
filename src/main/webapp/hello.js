0e6eea0b057502069c9ce2b202c313eb2c02ea58
viki9698 <rui.zhang9698@gmail.com>
2015-03-08 01:10:15 +0800

    Revert "Init repository"
    This reverts commit 6b965f5005a7fc07bec246498963a232103537ca.

--- src/main/webapp/hello.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp', [
- ])
- .controller("Hello", function($scope, $http) {
-	$http.get('http://rest-service.guides.spring.io/greeting').
-		success(function(data) {
-			$scope.greeting = data;
-		});
-});
